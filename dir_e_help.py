c80b9959396b1b2633c382cdf6abc1c090403667
fabriciocurvello <tiofab@gmail.com>
2022-10-17 23:10:36 -0300

    Initial commit

---
+++ dir_e_help.py
@@ -0,0 +1,17 @@
+"""
+dir e help
+
+dir -> mostra todos os atributos/propriedades e funções/métodos disponíveis para determinado tipo de variável ou dado.
+ex:
+    dir("Fabricio")
+
+help -> mostra as instruçÕes/manual de um aytributo/propriedade ou função/método.
+ex:
+help("Fabricio".lower)
+OBS: Utilize q para sair da tela de um help
+
+"""
+
+print(dir("Fabricio"))
+
+help("Fabricio".lower())
---
+++ pep8.py
@@ -0,0 +1,20 @@
+"""
+PEP8 - Python Enhancement Proposal (Proposta de aprimoramento do Python)
+
+1 - Utilize CamelCase para nomes de classes;
+
+2 - Utilize nomes em minúsculo, separados por underline, para funções ou variáveis;
+
+3 - Utilize 4 espaços para indentação! NÃO É RECOMENDADO UTILIZAR TAB.
+
+4 - Utilize linhas em branco após os itens, conforme estas definições:
+    - 2 linhas em branco para funções, definições e classes;
+    - 1 linha em branco para métodos;
+
+5 - Imports devem ser feitos sempre em linhas separadas, no topo do arquivo
+
+6 - Não deixe espaços em expressões e instruções
+
+7 - Termine sempre uma instrução com uma nova linha
+"""
+import this
